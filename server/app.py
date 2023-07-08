4007f20802077720b23bb956c2afa3f7697497c8
tgartland89 <thomas.gartland@gmail.com>
2023-07-08 17:08:39 -0600

    fixed code

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, make_response, request
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+
 from models import db, Apartment, Tenant, Lease
 
 app = Flask(__name__)
@@ -9,7 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
-
 api = Api(app)
 
 class IndexResource(Resource):
@@ -18,163 +18,110 @@ class IndexResource(Resource):
 
 api.add_resource(IndexResource, '/')
 
-# Apartment resource
+
 class ApartmentResource(Resource):
     def get(self, apartment_id=None):
         if apartment_id:
             apartment = Apartment.query.get(apartment_id)
-            if apartment:
-                return apartment.serialize()
-            else:
+            if not apartment:
                 return {'message': 'Apartment not found'}, 404
+            return apartment.to_dict(), 200
         else:
             apartments = Apartment.query.all()
-            return [apartment.serialize() for apartment in apartments]
+            return [apartment.to_dict() for apartment in apartments], 200
 
     def post(self):
-        data = request.get_json()
-        number = data.get('number')
-
-        if not number:
-            return {'message': 'Apartment number is required'}, 400
-
+        number = request.json.get('number')
         apartment = Apartment(number=number)
         db.session.add(apartment)
         db.session.commit()
+        return apartment.to_dict(), 201
 
-        return apartment.serialize(), 201
     def patch(self, apartment_id):
         apartment = Apartment.query.get(apartment_id)
-        if apartment:
-            data = request.get_json()
-            number = data.get('number')
-
-            if number:
-                apartment.number = number
-
-            db.session.commit()
-            return apartment.serialize()
-        else:
+        if not apartment:
             return {'message': 'Apartment not found'}, 404
-
+        number = request.json.get('number')
+        if number:
+            apartment.number = number
+        db.session.commit()
+        return apartment.to_dict(), 200
 
     def delete(self, apartment_id):
         apartment = Apartment.query.get(apartment_id)
-        if apartment:
-            db.session.delete(apartment)
-            db.session.commit()
-            return {'message': 'Apartment deleted'}
-        else:
+        if not apartment:
             return {'message': 'Apartment not found'}, 404
+        db.session.delete(apartment)
+        db.session.commit()
+        return {'message': 'Apartment deleted'}, 200
 
-api.add_resource(ApartmentResource, '/apartments', '/apartments/<int:apartment_id>')
 
-# Tenant resource
 class TenantResource(Resource):
     def get(self, tenant_id=None):
         if tenant_id:
             tenant = Tenant.query.get(tenant_id)
-            if tenant:
-                return tenant.serialize()
-            else:
+            if not tenant:
                 return {'message': 'Tenant not found'}, 404
+            return tenant.to_dict(), 200
         else:
             tenants = Tenant.query.all()
-            return [tenant.serialize() for tenant in tenants]
+            return [tenant.to_dict() for tenant in tenants], 200
 
     def post(self):
-        data = request.get_json()
-        name = data.get('name')
-        age = data.get('age')
-
-        if not name:
-            return {'message': 'Tenant name is required'}, 400
-
-        if age is None:
-            return {'message': 'Tenant age is required'}, 400
-
-        try:
-            tenant = Tenant(name=name, age=age)
-            db.session.add(tenant)
-            db.session.commit()
-            return tenant.serialize(), 201
-        except ValueError as e:
-            return {'message': str(e)}, 400
+        name = request.json.get('name')
+        age = request.json.get('age')
+        apartment_id = request.json.get('apartment_id')
+        tenant = Tenant(name=name, age=age, apartment_id=apartment_id)
+        db.session.add(tenant)
+        db.session.commit()
+        return tenant.to_dict(), 201
 
     def patch(self, tenant_id):
         tenant = Tenant.query.get(tenant_id)
-        if tenant:
-            data = request.get_json()
-            name = data.get('name')
-            age = data.get('age')
-
-            if name:
-                tenant.name = name
-
-            if age is not None:
-                try:
-                    tenant.age = age
-                except ValueError as e:
-                    return {'message': str(e)}, 400
-
-            db.session.commit()
-            return tenant.serialize()
-        else:
+        if not tenant:
             return {'message': 'Tenant not found'}, 404
+        name = request.json.get('name')
+        age = request.json.get('age')
+        apartment_id = request.json.get('apartment_id')
+        if name:
+            tenant.name = name
+        if age:
+            tenant.age = age
+        if apartment_id:
+            tenant.apartment_id = apartment_id
+        db.session.commit()
+        return tenant.to_dict(), 200
 
     def delete(self, tenant_id):
         tenant = Tenant.query.get(tenant_id)
-        if tenant:
-            db.session.delete(tenant)
-            db.session.commit()
-            return {'message': 'Tenant deleted'}
-        else:
-            return {'message': 'Tenant not found'}, 404
-
-api.add_resource(TenantResource, '/tenants', '/tenants/<int:tenant_id>')
-
-# Lease resource
-class LeaseResource(Resource):
-    def post(self):
-        data = request.get_json()
-        tenant_id = data.get('tenant_id')
-        apartment_id = data.get('apartment_id')
-        rent = data.get('rent')
-
-        if not tenant_id:
-            return {'message': 'Tenant ID is required'}, 400
-
-        if not apartment_id:
-            return {'message': 'Apartment ID is required'}, 400
-
-        if not rent:
-            return {'message': 'Rent amount is required'}, 400
-
-        tenant = Tenant.query.get(tenant_id)
-        apartment = Apartment.query.get(apartment_id)
-
         if not tenant:
             return {'message': 'Tenant not found'}, 404
+        db.session.delete(tenant)
+        db.session.commit()
+        return {'message': 'Tenant deleted'}, 200
 
-        if not apartment:
-            return {'message': 'Apartment not found'}, 404
 
-        lease = Lease(tenant=tenant, apartment=apartment, rent=rent)
+class LeaseResource(Resource):
+    def post(self, apartment_id):
+        rent = request.json.get('rent')
+        lease = Lease(rent=rent, apartment_id=apartment_id)
         db.session.add(lease)
         db.session.commit()
+        return lease.to_dict(), 201
 
-        return lease.serialize(), 201
-
-    def delete(self, lease_id):
-        lease = Lease.query.get(lease_id)
-        if lease:
-            db.session.delete(lease)
-            db.session.commit()
-            return {'message': 'Lease deleted'}
-        else:
+    def delete(self, apartment_id):
+        lease = Lease.query.filter_by(apartment_id=apartment_id).first()
+        if not lease:
             return {'message': 'Lease not found'}, 404
+        db.session.delete(lease)
+        db.session.commit()
+        return {'message': 'Lease deleted'}, 200
 
-api.add_resource(LeaseResource, '/leases', '/leases/<int:lease_id>')
+
+api.add_resource(ApartmentResource, '/apartments', '/apartments/<int:apartment_id>')
+api.add_resource(TenantResource, '/tenants', '/tenants/<int:tenant_id>')
+api.add_resource(LeaseResource, '/apartments/<int:apartment_id>/lease')
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,34 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
 
-class Apartment(db.Model):
+class Apartment(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.String(50), nullable=False)
-
-    tenants = db.relationship('Tenant', secondary='lease')
-    leases = db.relationship('Lease', backref='apartment')
-
-class Tenant(db.Model):
+    number = db.Column(db.String(10))
+    tenants = db.relationship('Tenant', backref='apartment', cascade='all, delete-orphan')
+    lease = db.relationship('Lease', backref='apartment', uselist=False, cascade='all, delete-orphan')
+    
+class Tenant(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-
-    apartments = db.relationship('Apartment', secondary='lease')
-    leases = db.relationship('Lease', backref='tenant')
+    name = db.Column(db.String(50))
+    age = db.Column(db.Integer)
+    apartment_id = db.Column(db.Integer, db.ForeignKey('apartment.id'))
 
     @validates('age')
     def validate_age(self, key, age):
         if age < 18:
-            raise ValueError("Tenant must be at least 18 years old.")
+            raise ValueError("Age must be over 18.")
         return age
 
-class Lease(db.Model):
+class Lease(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
-    rent = db.Column(db.Float, nullable=False)
-    tenant_id = db.Column(db.Integer, db.ForeignKey('tenant.id'), nullable=False)
-    apartment_id = db.Column(db.Integer, db.ForeignKey('apartment.id'), nullable=False)
-
-    tenant = db.relationship('Tenant', backref=db.backref('lease_associations', cascade='all, delete-orphan'))
-    apartment = db.relationship('Apartment', backref=db.backref('lease_associations', cascade='all, delete-orphan'))
+    rent = db.Column(db.Float)
+    apartment_id = db.Column(db.Integer, db.ForeignKey('apartment.id'))
